65b412a15c68b19b3118add13a80638686e68035
rashmi-22-stack <128853997+rashmi-22-stack@users.noreply.github.com>
2023-03-29 00:43:46 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+# Do not modify these lines
+__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
+__human_name__ = 'strings'
+
+# Add your code after this line
+scorer_name1 = "Ruud_Gullit",
+scorer_name2 =  "Marco_van_Baston"  
+scorer_names = scorer_name1 , scorer_name2
+print(scorer_names)
+
+txt = "Ruud Gullit scored in the <when_they_scored>nd minute"
+
+ 
+scored_0 = "till_32nd"    # no goal till 32nd_minutes of the game.
+scored_1 = "32"      # 1st_goal in 32nd_minute of the game.
+scored_2 = "8_minutes_before_second_half" # lead and won by 2nd goal of the game.
+total_scored = "scored_1 + scored_2"
+
+txt = "Ruud Gullit scored in the {}nd minute."
+print(txt.format(scored_1))
+
+txt = "Marco van Baston scored in the {} of game."
+print(txt.format(scored_2))
+
+
+report = ""
+
+
+player = "Ruud Gullit"   # player name as string 
+txt = player
+first_name = txt.find("Ruud")  # first name find_method
+last_name = "Gullit"
+ 
+print(first_name)
+print(len(last_name))   # length of last name
+print(txt[0]+"."+" "+last_name)    # name_short
+
+# chant
+x = 4
+print((txt[:4]+"!")*x)
+
+
+# good_chant:
+
+
+
+
+
